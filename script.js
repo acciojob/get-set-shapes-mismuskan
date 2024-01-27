b36468faf4f7c99e4db569f70a806c73560ff94a
Muskan Sheikh <144351188+mismuskan@users.noreply.github.com>
2024-01-27 18:14:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		get width(){
+			this.width=width;
+		}
+		get height{
+			this.height=height;
+		}
+		getArea()
+		{
+			let area=width*height;
+			return area;
+		}
+	}
+}
 
-class Square extends Animal {}
+class Square extends Animal {
+	super(height,width)
+	getPerimeter(){
+		let peri=(2*height)+(2*width);
+		return peri;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
